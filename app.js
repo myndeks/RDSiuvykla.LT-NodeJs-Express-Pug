7569f3b37fa6b7676d3cdbac4da7e31a751297ea
myndeks <56792714+myndeks@users.noreply.github.com>
2020-04-30 10:45:15 +0100

    update

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+var port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,6 +37,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
 });
 
 module.exports = app;
